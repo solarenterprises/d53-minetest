fd907cd721b5c7239688d214008f6836abc7f2c4
Nayiem <info@hodlercompany.com>
2022-05-23 19:40:17 +0200

    fix: use recipe for linux builder

--- buildlinux.sh
+++ buildlinux.sh
@@ -16,5 +16,6 @@ cp misc/district53.png AppDir/
 #wget -O appimage-builder-x86_64.AppImage https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.0-beta.1/appimage-builder-1.0.0-677acbd-x86_64.AppImage
 #chmod +x appimage-builder-x86_64.AppImage
 
-./appimage-builder-x86_64.AppImage AppDir
+./appimage-builder-x86_64.AppImage --recipe AppImageBuilder.yml
+
 
