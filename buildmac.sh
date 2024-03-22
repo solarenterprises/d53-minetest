697135e3c162f40bbb89777610688eb84b8662cd
MiniTurtle <miramanagames@gmail.com>
2024-03-22 16:23:44 +0200

    fix mac

--- buildmac.sh
+++ buildmac.sh
@@ -7,10 +7,11 @@ mkdir build/
 cd build/
 
 cmake .. \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
-    -DCMAKE_FIND_FRAMEWORK=LAST \
-    -DCMAKE_INSTALL_PREFIX=../build/macos/ \
-    -DRUN_IN_PLACE=FALSE -DENABLE_GETTEXT=TRUE
-
-make -j$(sysctl -n hw.logicalcpu)
-make package
+            -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
+            -DCMAKE_FIND_FRAMEWORK=LAST \
+            -DCMAKE_INSTALL_PREFIX=../build/macos/ \
+            -DRUN_IN_PLACE=FALSE -DENABLE_GETTEXT=TRUE \
+            -DINSTALL_DEVTEST=TRUE \
+            -DENABLE_REDIS=FALSE
+          cmake --build . -j$(sysctl -n hw.logicalcpu)
+make install
